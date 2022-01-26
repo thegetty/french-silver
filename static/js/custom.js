9e7cd91f6cfef0076c11432f08b97c755d5b3e2a
Greg Albers <galbers@getty.edu>
2022-01-26 13:38:03 -0800

    Add cat 7 and rtry a estructure of the headings and acordions in technical area

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,11 @@
 // From https://inclusive-components.design/collapsible-sections/
 
 (function() {
-  // Get all the <h3> headings
-  const headings = document.querySelectorAll('.content h3')
-  
+  // Get all the <h4> headings
+  const headings = document.querySelectorAll('.content h4')
+
   Array.prototype.forEach.call(headings, heading => {
-    // Give each <h3> a toggle button child
+    // Give each <h4> a toggle button child
     // with the SVG plus/minus icon
     heading.innerHTML = `
       <button aria-expanded="false">
@@ -16,16 +16,16 @@
         </svg>
       </button>
     `
-    
-    // Function to create a node list 
-    // of the content between this <h3> and the next
+
+    // Function to create a node list
+    // of the content between this <h4> and the next
     const getContent = (elem) => {
       let elems = []
-      while (elem.nextElementSibling && elem.nextElementSibling.tagName !== 'H3' && elem.nextElementSibling.tagName !== 'H2') {
+      while (elem.nextElementSibling && elem.nextElementSibling.tagName !== 'H4' && elem.nextElementSibling.tagName !== 'H3' && elem.nextElementSibling.tagName !== 'H2') {
         elems.push(elem.nextElementSibling)
         elem = elem.nextElementSibling
       }
-      
+
       // Delete the old versions of the content nodes
       elems.forEach((node) => {
         node.parentNode.removeChild(node)
@@ -33,34 +33,34 @@
 
       return elems
     }
-    
+
     // Assign the contents to be expanded/collapsed (array)
     let contents = getContent(heading)
-    
+
     // Create a wrapper element for `contents` and hide it
     let wrapper = document.createElement('div')
     wrapper.hidden = true
-    
+
     // Add each element of `contents` to `wrapper`
     contents.forEach(node => {
       wrapper.appendChild(node)
     })
-    
-    // Add the wrapped content back into the DOM 
+
+    // Add the wrapped content back into the DOM
     // after the heading
     heading.parentNode.insertBefore(wrapper, heading.nextElementSibling)
-    
+
     // Assign the button
     let btn = heading.querySelector('button')
-    
+
     btn.onclick = () => {
       // Cast the state as a boolean
       let expanded = btn.getAttribute('aria-expanded') === 'true' || false
-      
+
       // Switch the state
       btn.setAttribute('aria-expanded', !expanded)
       // Switch the content's visibility
-      wrapper.hidden = expanded    
+      wrapper.hidden = expanded
     }
   })
 })();
@@ -69,7 +69,7 @@
 (function() {
   // Get all the <h2> headings
   const headings = document.querySelectorAll('.content h2')
-  
+
   Array.prototype.forEach.call(headings, heading => {
     // Give each <h2> a toggle button child
     // with the SVG plus/minus icon
@@ -82,8 +82,8 @@
         </svg>
       </button>
     `
-    
-    // Function to create a node list 
+
+    // Function to create a node list
     // of the content between this <h2> and the next
     const getContent = (elem) => {
       let elems = []
@@ -91,7 +91,7 @@
         elems.push(elem.nextElementSibling)
         elem = elem.nextElementSibling
       }
-      
+
       // Delete the old versions of the content nodes
       elems.forEach((node) => {
         node.parentNode.removeChild(node)
@@ -99,34 +99,34 @@
 
       return elems
     }
-    
+
     // Assign the contents to be expanded/collapsed (array)
     let contents = getContent(heading)
-    
+
     // Create a wrapper element for `contents` and hide it
     let wrapper = document.createElement('div')
     wrapper.hidden = true
-    
+
     // Add each element of `contents` to `wrapper`
     contents.forEach(node => {
       wrapper.appendChild(node)
     })
-    
-    // Add the wrapped content back into the DOM 
+
+    // Add the wrapped content back into the DOM
     // after the heading
     heading.parentNode.insertBefore(wrapper, heading.nextElementSibling)
-    
+
     // Assign the button
     let btn = heading.querySelector('button')
-    
+
     btn.onclick = () => {
       // Cast the state as a boolean
       let expanded = btn.getAttribute('aria-expanded') === 'true' || false
-      
+
       // Switch the state
       btn.setAttribute('aria-expanded', !expanded)
       // Switch the content's visibility
-      wrapper.hidden = expanded    
+      wrapper.hidden = expanded
     }
   })
 })();
