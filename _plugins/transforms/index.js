f6d41b9f1be1864d2a2044a76a91cff6fff0e8c4
Greg Albers <galbers@getty.edu>
2022-10-19 11:19:18 -0700

    Add transform to wrap fractions in non-breaking span

--- _plugins/transforms/index.js
+++ _plugins/transforms/index.js
@@ -1,8 +1,13 @@
+//
+// CUSTOMIZED FILE -- French Silver
+// adds new fractions transform, lines 10 and 25
+//
 const chalk = require('chalk')
 const fs = require('fs-extra')
 const path = require('path')
 
 const formatOutput = require('./format')
+const wrapFractions = require('./fractions')
 const outputs = require('./outputs')
 
 /**
@@ -16,6 +21,8 @@ module.exports = function(eleventyConfig, collections) {
    * Registers a transform to format output using Prettier
    */
   eleventyConfig.addTransform('format', formatOutput)
+
+  eleventyConfig.addTransform('fractions', wrapFractions)
 
   /**
    * Register plugin to generate output for epub, html, and pdf using `transforms`
